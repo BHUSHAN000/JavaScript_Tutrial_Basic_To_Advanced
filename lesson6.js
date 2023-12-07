6b64e7a021157ff9b02456a81441ae1c31c7429e
BHUSHAN PAWAR <39292597+BHUSHAN000@users.noreply.github.com>
2023-12-08 00:27:27 +0530

    Create lesson6.js

---
+++ lesson6.js
@@ -0,0 +1,35 @@
+// Arithmetic Operators (+, -, *, /, %, ++, --)
+
+let x = 10;
+let y = 22;
+
+console.log(x + y);
+
+console.log(y - x);
+
+console.log(x * y);
+
+console.log(y / x);
+
+console.log(y % x); // Remainder
+
+x++;    // x = x + 1
+console.log(x); 
+
+console.log(y++);
+console.log(y);
+
+console.log(++x)
+console.log(++y)
+
+// Assignment Operators (=, +=, -=, *=, /=, %=)
+
+let salary = 100;
+
+salary += 1000; // salary = salary + 1000
+console.log(salary);
+
+salary -= 1000; // salary = salary - 1000
+console.log(salary);
+
+
