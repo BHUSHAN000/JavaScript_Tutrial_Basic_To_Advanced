be5a6abbde2a6949034ed762d6eed1319d48d6cf
BHUSHAN PAWAR <39292597+BHUSHAN000@users.noreply.github.com>
2024-01-25 23:46:13 +0530

    Update lesson34.js

--- lesson34.js
+++ lesson34.js
@@ -1,102 +1,101 @@
 
 // String Datatype
 
-// 2 ways
+// 2 ways to create a string
 
-// 1) literal
+// 1) using string literal
 
-// let newStr = 'Hello World';
+let newString = 'Hello World';
 
-// 2) constructor
+// 2) using string constructor
 
-// let newStr2 = new String('Hello World');
+let newString2 = new String('Hello World');
 
-// console.log(newStr == newStr2); // true // loose equality
-// console.log(newStr === newStr2); // false // strict equality
-// console.log(typeof newStr, typeof newStr2);
+// console.log(newString);
+// console.log(newString2);
+// console.log(newString == newString2); // true => // loose equality operator
+// console.log(newString === newString2); // false => // strict equality operator
+// console.log(typeof newString, typeof newString2); // string object
+//=================================================================================
 
-// String Properties
-
-// 1) length
-
-// console.log(newStr.length);
-
-
-// string denotation
+// 3 ways to denote a string
 
 // 1) single quote
-// let str1 = 'Hello World';
-// console.log(str1);
+// let singleQuote = 'Hello\nWorld';
+// console.log(singleQuote);
 // // 2) double quote
-// let str2 = "Hello world";
-// console.log(str2);
+// let doubleQuote = "Hello\nWorld";
+// console.log(doubleQuote);
 // // 3) backtick
-// let str3 = `Hello
+// let backtick = `Hello
 // World`;
-// console.log(str3);
+// console.log(backtick);
 
-// Static Methods
+
+// let singleQuote1 = 'Hello \"World\"';
+// console.log(singleQuote1);
+// // 2) double quote
+// let doubleQuote1 = "Hello \'World\'";
+// console.log(doubleQuote1);
+// // 3) backtick
+// let backtick1 = `Hello \"World\"`;
+// console.log(backtick1);
+//=================================================================================
+
+// string property
+
+// 1) length property
+// console.log(newString.length); // 11
+
+
+//=================================================================================
+
+// static methods: methods that are called directly on the constructor
 
 //1)  String.fromCharCode() // returns a string created by using the specified sequence of Unicode values => BMP Code (Basic Multilingual Plane)
 // 0 - 65535
 
-// let str4 = String.fromCharCode(65, 66, 67);
-// console.log(str4);
-// let str5 = String.fromCharCode(68);
-// console.log(str5);
-// let str8 = String.fromCharCode(0x1f609);
-// console.log(str8)
+// console.log(String.fromCharCode(65,66,67,68))
+// console.log(String.fromCharCode(66))
+// console.log(String.fromCharCode(90))
+// console.log(String.fromCharCode(0x1f609));
+// console.log(String.fromCharCode(0x01d4bd));
 
-// let str10 = String.fromCharCode(0x01d4bd);
-// console.log(str10)
 //2)  String.fromCodePoint() // returns a string created by using the specified sequence of code points => Unicode Code Point or Non BMP Code (Basic Multilingual Plane)
 // 0 - 1114111
 
-// let str6 = String.fromCodePoint(65, 66, 67);
-// console.log(str6);
-// let str7 = String.fromCodePoint(68);
-// console.log(str7);
-// let str9 = String.fromCodePoint(0x1f609);
-// console.log(str9);
+// console.log(String.fromCodePoint(65));
+// console.log(String.fromCodePoint(66));
+// console.log(String.fromCodePoint(90));
+// console.log(String.fromCodePoint(0x1f609));
+// console.log(String.fromCodePoint(0x01d4bd));
 
-// let str11 = String.fromCodePoint(0x01d4bd);
-// console.log(str11);
-
+//=================================================================================
 //3)  String.raw => return a raw string representation of a template literal
 
-
-// escape sequence or escape character
+// escape character
 // \n => new line
 // \t => tab
 // \b => backspace
+// \r => carriage return
+// \v => vertical tab
+// \f => form feed
 // \" => double quote
 // \' => single quote
 
-// let str12 = String.raw`Hello\"World\"`;
+// console.log(`Hello\nWorld`);
+// console.log(String.raw`Hello\nWorld`);
+console.log(`c:\dev\notes\lesson34.js`)
+console.log(String.raw`c:\dev\notes\lesson34.js`);
 
-// console.log(str12);
+//=================================================================================
 
-// console.log('Hello\nWorld');
-// console.log('Hello\tWorld');
-// console.log('Hello\"World\"');
-// console.log("C:Users\notissueDesktopJavaScriptlesson34.js");
+// template literal => backtick
 
-// let str13 = String.raw`C:\Users\notissue\Desktop\JavaScript\lesson34.js`;
-// console.log(str13);
+let myName = `Bhushan Pawar`
 
-// template literal => it means we can use multi line string using backtick
+// string interpolation => it means we can inject a variable or expression inside a string
 
-// let str14 = `Hello
-// World`;
-// console.log(str14);
-
-// string interpolation => it means we can use variable inside the string
-// let temp = 20
-// let str15 = 'Hello ' + temp + 10 + ' World';
-// console.log(str15);
-
-// // let str16 = `Hello ${temp} World`;
-// // console.log(str16);
-
-// let str17 = `Hello ${temp + 10} World`;
-// console.log(str17);
+console.log(`hello ${myName}`)
+console.log(`hello ${myName.toUpperCase()}`)
+console.log(`hello ${20     + 30}`)
