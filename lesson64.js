b288cac25940f1ad5876e4a261f4aa5c23a4590d
BHUSHAN PAWAR <39292597+BHUSHAN000@users.noreply.github.com>
2024-06-19 23:22:30 +0530

    Create lesson64.js

---
+++ lesson64.js
@@ -0,0 +1,8 @@
+
+const headingTag = document.getElementById("headingId");
+console.log(headingTag);
+headingTag.style.color = "red";
+headingTag.style.fontSize = "50px";
+headingTag.style.fontFamily = "Arial";
+headingTag.innerHTML = '<span>Bye</span>';
+// headingTag.innerText = "Hello World";
