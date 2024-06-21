84e0c84dce11b949f2742a5535a4988abf8bcc26
BHUSHAN PAWAR <39292597+BHUSHAN000@users.noreply.github.com>
2024-06-21 23:06:44 +0530

    Create lesson66.js

---
+++ lesson66.js
@@ -0,0 +1,7 @@
+
+const paragraphs = document.getElementsByTagName('p');
+console.log(paragraphs);
+
+paragraphs[0].style.color = 'red';
+paragraphs[1].style.color = 'blue';
+paragraphs[2].style.fontSize = '22px';
