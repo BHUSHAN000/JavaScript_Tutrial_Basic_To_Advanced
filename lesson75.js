20e79277d21f5377ad42d9e5b3be4866a95f34c2
BHUSHAN PAWAR <39292597+BHUSHAN000@users.noreply.github.com>
2024-07-22 23:49:49 +0530

    Create lesson75.js

---
+++ lesson75.js
@@ -0,0 +1,18 @@
+
+const parentEl = document.querySelector(".outerClass");
+// 1. firstElementChild
+
+console.log(parentEl.firstElementChild);    // it returns the first element child of the parent element
+
+// 2. lastElementChild
+
+console.log(parentEl.lastElementChild);    // it returns the last element child of the parent element
+
+
+// 3. firstChild
+
+console.log(parentEl.firstChild);    // it returns the first child node of targeted element i.e node means text,comment,HTML element
+
+// 4. lastChild
+
+console.log(parentEl.lastChild);    // it returns the last child node of targeted element i.e node means text,comment,HTML element
