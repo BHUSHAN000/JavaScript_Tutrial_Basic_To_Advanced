54b27b5a4b93245399aac790db951bee544b43ba
BHUSHAN PAWAR <39292597+BHUSHAN000@users.noreply.github.com>
2024-10-06 00:25:09 +0530

    Create lesson84.js

---
+++ lesson84.js
@@ -0,0 +1,6 @@
+
+console.log("innerHeight", window.innerHeight);
+console.log('outerHeight', window.outerHeight);
+
+console.log("innerWidth", window.innerWidth);
+console.log('outerWidth', window.outerWidth);
