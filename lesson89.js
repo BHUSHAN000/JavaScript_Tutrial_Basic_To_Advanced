9a015fe502d45d0163b721960c86c4ec4176d7e6
BHUSHAN PAWAR <39292597+BHUSHAN000@users.noreply.github.com>
2024-10-12 23:40:17 +0530

    Create lesson89.js

---
+++ lesson89.js
@@ -0,0 +1,21 @@
+
+// console.log(window.location)
+console.log("host", location.host);
+console.log("hostname", location.hostname);
+console.log("href", location.href);
+console.log("origin", location.origin);
+console.log("pathname", location.pathname);
+console.log("port", location.port);
+console.log("protocol", location.protocol);
+console.log("search", location.search);
+
+// location.reload();
+
+// location.assign("https://www.google.com");
+// location.replace("https://www.google.com");
+
+
+// assign() method loads a new document.
+// replace() method replaces the current document with a new one.
+
+// assign vs replace => assign() method loads a new document, replace() method replaces the current document with a new one. The difference is that assign() keeps the current document in the session history, meaning the user can go back to it, while replace() does not.
