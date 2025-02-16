6c0aab1deb1a62460308ba4e94486ab79f3668a6
BHUSHAN PAWAR <39292597+BHUSHAN000@users.noreply.github.com>
2025-02-16 13:22:47 +0530

    Update lesson99.js

--- lesson99.js
+++ lesson99.js
@@ -26,15 +26,15 @@ function getData() {
 // AbortSignal => interface represents a signal object that allows you to communicate with a DOM request (such as a Fetch) and abort it if required.
 // AbortSignal.timeout() => method creates a new AbortSignal object instance, which will be aborted after the specified number of milliseconds.
 
-function getData() {
-  fetch("https://jsonplaceholder.typicode.com/albums", {
-    signal: AbortSignal.timeout(3000),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
+// function getData() {
+//   fetch("https://jsonplaceholder.typicode.com/albums", {
+//     signal: AbortSignal.timeout(3000),
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// }
